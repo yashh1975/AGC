c577b95e2e9dbdcc649658e0879821147db012d2
yashh1975 <yashwanthkumarr2005@gmail.com>
2026-08-28 18:54:53 +0530

    update-facebook-link-v3

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -12,7 +12,7 @@ export const business = {
   whatsappMessage:
     "Hello Anjaneya Gold Company, I would like to enquire about your gold services.",
   email: "anjaneyagoldcompany@gmail.com",
-  facebook: "https://www.facebook.com/share/14muWN5xhMJ/",
+  facebook: "https://www.facebook.com/share/1dTzBBy1Pv/",
   instagram: "https://www.instagram.com/anjaneyagoldcompany",
   addressLines: [
     "#20, 1st Floor,",
